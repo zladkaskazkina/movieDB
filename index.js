36b5e54e2a0d719130e817cf112660473e90fe25
Vladi Benesova <vladi.benesova1@gmail.com>
2020-05-27 14:00:34 +0200

    complete all tasks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 console.log('hi!');
+
 const movieDB = {
     movies: [
         "Logan",
@@ -10,72 +11,14 @@ const movieDB = {
     ]
 };
 
-const personalMovieDB = {
-    count: 0,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-    start: function () {
-        personalMovieDB.count = +prompt('How many films have you seen in last month?', '');
-        while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
-            personalMovieDB.count = +prompt('How many films have you seen in last month?', '');
-        }
-    },
-    rememberMyFilms: function () {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt('One of the last films that you have seen:'),
-                b = prompt('Your score of this film:');
-            //check if the answer is valid:
-            if (a != '' && b != '' && a != null && b != null && a.length < 50) {
-                personalMovieDB.movies[a] = b;
-            } else {
-                i--; // come back to one step and ask again
-            }
-        }
-    },
-    personalLevel: function () {
-        if (personalMovieDB.count < 10) {
-            console.log('You are not a fun of movies');
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-            console.log('You have seen as much movies as average american person');
-        } else if (personalMovieDB.count > 30) {
-            console.log('You really like to watch movies');
-        } else {
-            console.log('Probably, it is an error');
-        }
-    },
-    showMyDB: function (hidden) {
-        if (!hidden) {
-            console.log(personalMovieDB);
-        }
-    },
-    toggleVisibleMyDB: function () {
-        if (personalMovieDB.privat) {
-            personalMovieDB.privat = true;
-        } else {
-            personalMovieDB.privat = false;
-        }
-    },
-    writeYourGenres: function () {
-        for (let i = 1; i < 4; i++) {
-            let genre = prompt(`Your favorite genre number ${i}`);
-            if (genre == '' || genre == null) {
-                console.log('You forgot to write the answer');
-                i--;
-            } else { personalMovieDB.genres[i - 1] = genre; }
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            console.log(`Your favorite genre ${i + 1} is ${item}`);
-        });
-    }
-
-};
 //delete ads block
 const adv = document.querySelectorAll('.promo__adv img');
-adv.forEach(item => {
-    item.remove();
-});
+const deleteAds = (arr) => {
+    arr.forEach(item => {
+        item.remove();
+    });
+};
+deleteAds(adv);
 
 //change background image
 const poster = document.querySelector('.promo__bg');
@@ -86,19 +29,58 @@ poster.style.backgroundImage = 'url("img/bg.jpg")';
 const changedGenre = poster.querySelector('.promo__genre');
 changedGenre.textContent = 'Drama';
 
-//clean movies list
-const movieList = document.querySelector('.promo__interactive-list');
-movieList.innerHTML = '';
-
 //sort movies
-movieDB.movies.sort();
+const sortArr = (arr) => {
+    arr.sort();
+};
 
-//add movies from object list
-movieDB.movies.forEach((film, i) => {
-    movieList.innerHTML += `
+const movieList = document.querySelector('.promo__interactive-list');
+
+function createMovieList(films, parent) {
+    //clean movies list
+    parent.innerHTML = '';
+    //sort the list
+    sortArr(films);
+    //add movies from object list
+    films.forEach((film, i) => {
+        parent.innerHTML += `
                         <li class="promo__interactive-item">
                             ${i + 1}. ${film}
                             <div class="delete"></div>
                         </li>`;
+    });
+    //delete elements when you click on trash
+    document.querySelectorAll('.delete').forEach((btn, i) => {
+        btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            createMovieList(movieDB.movies, movieList); //recursion
+        });
+    });
+}
+createMovieList(movieDB.movies, movieList);
+
+//add new film to array
+const addForm = document.querySelector('form.add'),
+    addInput = addForm.querySelector('.adding__input'),
+    checkForm = addForm.querySelector('[type="checkbox"]');
+
+addForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const favorite = checkForm.checked;
+    if (addInput.value) {
+        if (favorite) {
+            console.log('favorite!');
+        }
+        if (addInput.value.length > 21) {
+            addInput.value = `${addInput.value.substring(0, 22)}...`;
+        }
+
+        movieDB.movies.push(addInput.value);
+        sortArr(movieDB.movies);
+        createMovieList(movieDB.movies, movieList);
+    }
+    event.target.reset(); //clean the input
 });
+
 
